3ff90214aacad63f4acbe4545829796c5c2ed20f
Djinada12 <95240487+Djinada12@users.noreply.github.com>
2025-03-15 15:01:42 +0300

    first commit

---
+++ BVT_demo/src/test/java/ru/mtuci/BVT_demo/BvtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.BVT_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
